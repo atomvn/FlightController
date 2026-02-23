d1c043348cb2476da46e4385bbfec4e4831b134c
CerasCTK <ceras410@gmail.com>
2026-02-23 22:33:09 +0700

    refactor(build): replace '=' with ':=' for FreeRTOS directory variables
    Use immediate expansion to avoid unintended re-evaluation if
    FREERTOS_DIR is modified later in the file.

--- config/makefile_freertos.mk
+++ config/makefile_freertos.mk
@@ -1,8 +1,8 @@
 # source director
-FREERTOS_SRC_DIR     = $(FREERTOS_DIR)
-FREERTOS_INC_DIR     = $(FREERTOS_DIR)/include
-FREERTOS_ARM_CM3_DIR = $(FREERTOS_DIR)/portable/GCC/ARM_CM3
-FREERTOS_MemMang_DIR = $(FREERTOS_DIR)/portable/MemMang
+FREERTOS_SRC_DIR     := $(FREERTOS_DIR)
+FREERTOS_INC_DIR     := $(FREERTOS_DIR)/include
+FREERTOS_ARM_CM3_DIR := $(FREERTOS_DIR)/portable/GCC/ARM_CM3
+FREERTOS_MemMang_DIR := $(FREERTOS_DIR)/portable/MemMang
 
 # add freertos source
 SRC  += $(FREERTOS_SRC_DIR)/list.c
@@ -15,5 +15,3 @@ SRC  += $(FREERTOS_MemMang_DIR)/heap_4.c
 # include directories
 INCLUDE_DIRS += $(FREERTOS_INC_DIR)
 INCLUDE_DIRS += $(FREERTOS_ARM_CM3_DIR)
-
-
