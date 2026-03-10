d4c3b5485fa0990f02ce248a9177073f32d34dbf
atomvn <hnvanhao@gmail.com>
2026-03-10 23:32:35 +0700

    feat: add uart driver with APIs

---
+++ src/driver/uart.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <libopencm3/cm3/common.h>
+#include <libopencm3/stm32/f1/memorymap.h>
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/usart.h>
+
+#include "uart.h"
+
+void uart_init(void) {
+    rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_USART1);
+
+	// UART TX on PA9 (GPIO_USART1_TX)
+	gpio_set_mode(GPIOA,
+		GPIO_MODE_OUTPUT_50_MHZ,
+		GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
+		GPIO_USART1_TX);
+
+	usart_set_baudrate(USART1,38400);
+	usart_set_databits(USART1,8);
+	usart_set_stopbits(USART1,USART_STOPBITS_1);
+	usart_set_mode(USART1,USART_MODE_TX);
+	usart_set_parity(USART1,USART_PARITY_NONE);
+	usart_set_flow_control(USART1,USART_FLOWCONTROL_NONE);
+	usart_enable(USART1);
+}
+
+void uart_send_char(char c) {
+	usart_send_blocking(USART1, c);
+}
+
+void uart_send_string(const char *str) {
+	while (*str) {
+		usart_send_blocking(USART1, *str++);
+	}
+}
+
+void uart_send_uint32(uint32_t value) {
+	char buf[10];
+    int i = 0;
+
+    if (value == 0) {
+        uart_send_char('0');
+        return;
+    }
+
+    while (value > 0) {
+        buf[i++] = '0' + (value % 10);
+        value /= 10;
+    }
+
+    while (i--) {
+        uart_send_char(buf[i]);
+    }
+}
+
+void uart_send_int32(int32_t value)
+{
+    if (value < 0) {
+        uart_send_char('-');
+        value = -value;
+    }
+
+    uart_send_uint32((uint32_t)value);
+}
+
+void uart_send_float(float value)
+{
+    if (value < 0) {
+        uart_send_char('-');
+        value = -value;
+    }
+
+    uint32_t integer = (uint32_t)value;
+    uint32_t fraction = (uint32_t)((value - integer) * 1000); // 3 decimal
+
+    uart_send_uint32(integer);
+    uart_send_char('.');
+
+    if (fraction < 100) uart_send_char('0');
+    if (fraction < 10) uart_send_char('0');
+
+    uart_send_uint32(fraction);
+}
+
+void uart_send_hex(uint32_t value)
+{
+    char hex[] = "0123456789ABCDEF";
+
+    uart_send_string("0x");
+
+    for (int i = 28; i >= 0; i -= 4) {
+        uart_send_char(hex[(value >> i) & 0xF]);
+    }
+}
+
+void uart_printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    while (*fmt) {
+
+        if (*fmt != '%') {
+            uart_send_char(*fmt++);
+            continue;
+        }
+
+        fmt++;
+
+        switch (*fmt) {
+
+        case 'd':
+            uart_send_int32(va_arg(args, int32_t));
+            break;
+
+        case 'u':
+            uart_send_uint32(va_arg(args, uint32_t));
+            break;
+
+        case 'x':
+            uart_send_hex(va_arg(args, uint32_t));
+            break;
+
+        case 'f':
+            uart_send_float((float)va_arg(args, double));
+            break;
+
+        case 's':
+            uart_send_string(va_arg(args, char *));
+            break;
+
+        case 'c':
+            uart_send_char((char)va_arg(args, int));
+            break;
+
+        default:
+            uart_send_char('%');
+            uart_send_char(*fmt);
+        }
+
+        fmt++;
+    }
+
+    va_end(args);
+}
+
---
+++ src/driver/uart.h
@@ -0,0 +1,13 @@
+#ifndef UART_H
+#define UART_H
+
+void uart_init(void);
+void uart_send_char(char c);
+void uart_send_string(const char *str);
+void uart_send_uint32(uint32_t value);
+void uart_send_int32(int32_t value);
+void uart_send_float(float value);
+void uart_send_hex(uint32_t value);
+void uart_printf(const char *format, ...);
+
+#endif
