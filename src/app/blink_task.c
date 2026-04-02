a7adf434f5f38dc06cc4a00313f124a33fef9a79
atomvn <hnvanhao@gmail.com>
2026-04-02 23:08:30 +0700

    feat: add blink task to verify the system is running

--- src/app/blink_task.c
+++ src/app/blink_task.c
@@ -9,6 +9,6 @@
 void blink_task(void *pvParameters) {
     while (1) {
         gpio_toggle(GPIOC, GPIO13);
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
     }
 }
